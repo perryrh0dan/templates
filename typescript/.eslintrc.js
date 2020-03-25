38f0f3fd52a7b62397cace796b9287fb33ea904e
Thomas Pöhlmann <thomaspoehlmann96@googlemail.com>
2020-03-25 13:22:58 +0100

    fix(typescript): small tempalte update

--- typescript/.eslintrc.js
+++ typescript/.eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
   plugins: ["@typescript-eslint"],
-  // extends: [
-  //   'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  // ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
@@ -27,10 +28,6 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-member-accessibility": ["error"],
     "no-inferrable-types": [0, "always"],
-    "semi": [2, "always"],
-    "quotes": ["error", "single"],
-    "space-before-function-paren": [2, "never"],
-    "comma-spacing": ["error", { "before": false, "after": true }]
   },
   overrides: [
     {
