c388ccdfc131ad3489a9357b0a8c0e8600c9aa5f
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-08-21 13:58:53 +0200

    feat(rust): add better rust template

---
+++ rust/target/rls/debug/deps/rust-0dfcb876f5df3371.d
@@ -0,0 +1,5 @@
+/home/thomas/dev/templates/rust/target/rls/debug/deps/rust-0dfcb876f5df3371.rmeta: src/main.rs
+
+/home/thomas/dev/templates/rust/target/rls/debug/deps/rust-0dfcb876f5df3371.d: src/main.rs
+
+src/main.rs:
