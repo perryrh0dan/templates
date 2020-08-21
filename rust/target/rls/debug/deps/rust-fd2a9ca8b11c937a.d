bf3522a42642e4a853882b80d8e4096849c43667
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-08-21 14:01:28 +0200

    fix(rust): add gitignore

--- rust/target/rls/debug/deps/rust-fd2a9ca8b11c937a.d
+++
@@ -1,5 +0,0 @@
-/home/thomas/dev/templates/rust/target/rls/debug/deps/rust-fd2a9ca8b11c937a.rmeta: src/main.rs
-
-/home/thomas/dev/templates/rust/target/rls/debug/deps/rust-fd2a9ca8b11c937a.d: src/main.rs
-
-src/main.rs:
